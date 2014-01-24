3a9c5b68bce778e88063336aef12b2e48bc81f6b
Aria Haghighi <me@aria42.com>
2014-01-23 16:20:02 -0800

    Update IDifferentiableFn.java
    Comment addition

--- src/main/java/edu/umass/nlp/optimize/IDifferentiableFn.java
+++ src/main/java/edu/umass/nlp/optimize/IDifferentiableFn.java
@@ -3,7 +3,8 @@ package edu.umass.nlp.optimize;
 import edu.umass.nlp.utils.IPair;
 
 /**
- *
+ * A differentiable function is one that can take a vector of
+ * numbers and return the pair f(x) and gradient of f at x
  */
 public interface IDifferentiableFn {
   public IPair<Double,double[]> computeAt(double[] x);
